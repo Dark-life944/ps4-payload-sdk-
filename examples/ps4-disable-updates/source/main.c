859653aa45b4300a8ad07d08f1e7af2f8c70d17b
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-06-13 22:14:13 +0100

    Update main.c

--- examples/ps4-disable-updates/source/main.c
+++ examples/ps4-disable-updates/source/main.c
@@ -1,40 +1,98 @@
-//#define DEBUG_SOCKET
-#define DEBUG_IP "192.168.2.2"
+#define DEBUG_SOCKET
+#define DEBUG_IP "192.168.100.16"
 #define DEBUG_PORT 9023
 
 #include "ps4.h"
+#include "syscall.h"
 
-int _main(struct thread *td) {
-  UNUSED(td);
+#define SYS_NAMEDOBJ_CREATE 557
+#define SYS_NAMEDOBJ_DELETE 558
 
-  initKernel();
-  initLibc();
+int _main(struct thread *td)
+{
+    UNUSED(td);
 
-#ifdef DEBUG_SOCKET
-  initNetwork();
-  DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
-#endif
-
-  jailbreak();
-
-  initSysUtil();
-
-  unlink("/update/PS4UPDATE.PUP.net.temp");
-  rmdir("/update/PS4UPDATE.PUP.net.temp");
-  mkdir("/update/PS4UPDATE.PUP.net.temp", 777);
-
-  unlink("/update/PS4UPDATE.PUP");
-  rmdir("/update/PS4UPDATE.PUP");
-  mkdir("/update/PS4UPDATE.PUP", 777);
-
-  char fw_version[6] = {0};
-  get_firmware_string(fw_version);
-  printf_notification("Disabled updates!\nPS4 Firmware %s", fw_version);
+    initKernel();
+    initLibc();
 
 #ifdef DEBUG_SOCKET
-  printf_debug("Closing socket...\n");
-  SckClose(DEBUG_SOCK);
+    initNetwork();
+    DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
 #endif
 
-  return 0;
+    jailbreak();
+    initSysUtil();
+
+    int handles[8];
+    int fd;
+
+    printf_notification("=== Generation Wraparound ===");
+
+    // object 
+    fd = kqueue();
+
+    int h0 = syscall(
+        SYS_NAMEDOBJ_CREATE,
+        "poc",
+        fd,
+        0x107
+    );
+
+    printf_debug("Initial handle: 0x%x\n", h0);
+    printf_notification("Initial handle: 0x%x", h0);
+
+
+    // delete + recreate
+    for (int i = 0; i < 8; i++)
+    {
+        syscall(
+            SYS_NAMEDOBJ_DELETE,
+            h0 & 0x1fff,
+            0x107
+        );
+
+        fd = kqueue();
+
+        handles[i] = syscall(
+            SYS_NAMEDOBJ_CREATE,
+            "poc",
+            fd,
+            0x107
+        );
+
+        printf_debug(
+            "Iteration %d handle: 0x%x\n",
+            i,
+            handles[i]
+        );
+    }
+
+
+    // 3) check wrap
+    printf_debug(
+        "Original: 0x%x After wrap: 0x%x\n",
+        h0,
+        handles[7]
+    );
+
+
+    if ((handles[7] & 0xffff) == (h0 & 0xffff))
+    {
+        printf_notification(
+            "WRAPAROUND CONFIRMED!"
+        );
+    }
+    else
+    {
+        printf_notification(
+            "No wrap"
+        );
+    }
+
+
+#ifdef DEBUG_SOCKET
+    SckClose(DEBUG_SOCK);
+#endif
+
+    return 0;
 }
