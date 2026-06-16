7557b8f680e6e0c5492c296b84a414fc03111185
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-06-16 23:06:29 +0100

    Update main.c

--- examples/ps4-app2usb/source/main.c
+++ examples/ps4-app2usb/source/main.c
@@ -15,7 +15,7 @@ int _main(struct thread *td) {
     // name بطول 0x20 بدون null terminator
     // copyinstr يكتب 0x21 bytes
     char name[0x21];
-    memset(name, 0x41, 0x20);  // 'A' * 32
+    memset(name, 0x41, 0x19);  // 'A' * 32
     // لا نضع null! → copyinstr يقرأ byte إضافية
 
     printf_debug("creating socket ...\n");
